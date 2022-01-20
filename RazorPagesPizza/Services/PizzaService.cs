f670f700de3fad6f58538ed94173fec2facccb7d
Marcello Bozzi <bozzimarcello@gmail.com>
2022-01-20 10:16:14 +0100

    Aggiunto accesso al DB con Microsoft.Data.SqlClient

--- RazorPagesPizza/Services/PizzaService.cs
+++ RazorPagesPizza/Services/PizzaService.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
 
 namespace RazorPagesPizza.Services
 {
@@ -12,11 +13,39 @@ namespace RazorPagesPizza.Services
         static int nextId = 3;
         static PizzaService()
         {
+            /*
             Pizzas = new List<Pizza>
             {
                 new Pizza { Id = 1, Name = "Classic Italian", Price=20.00M, Size=PizzaSize.Large, IsGlutenFree = false },
                 new Pizza { Id = 2, Name = "Veggie", Price=15.00M, Size=PizzaSize.Small, IsGlutenFree = true }
             };
+            */
+            Pizzas = new List<Pizza>();
+
+            string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=DBPizzeria;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                String sql = "SELECT * FROM TBPizza";
+
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Pizzas.Add(new Pizza { Id = reader.GetInt32(0),
+                                Name = reader.GetString(1), 
+                                Size = (PizzaSize) reader.GetInt16(2),
+                                IsGlutenFree = reader.GetBoolean(3), 
+                                Price = reader.GetDecimal(4)
+                            });
+                        }
+                    }
+                }
+            }
         }
 
         public static List<Pizza> GetAll() => Pizzas;
